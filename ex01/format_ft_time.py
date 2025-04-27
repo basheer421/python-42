2deac5ad9504cbbdc9473043d5bbb87a2f68c962
basheer421 <basheer123b2@gmail.com>
2025-04-27 15:51:03 +0400

    Fix

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -3,6 +3,6 @@ import datetime
 time_now = datetime.datetime.now()
 timestamp = time_now.timestamp()
 print("Seconds since January 1, 1970:",
-        "{:,f}".format(timestamp),
-        "or {:,.2e}".format(timestamp))
+        "{:,.4f}".format(timestamp),
+        "or {:.2e} in scientific notation".format(timestamp))
 print(time_now.strftime('%b %d %Y'))
